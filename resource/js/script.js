67a8686659027e5281c787125c8874a458481904
Anna Vihrogonova <annavihrogonova@yahoo.com>
2017-08-08 12:41:50 +0100

    Add sticky navigation to project

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
-	
+	$('.js-section-about-me').waypoint(function(direction) {
+		if(direction == 'down') {
+			$('nav').addClass('sticky-nav');
+		}else {
+			$('nav').removeClass('sticky-nav');
+		}
+	},  {
+ 	 offset: '60px'
+	});
+
+
+
+// 	var waypoints = $('#handler-first').waypoint(function(direction) {
+//   notify(this.element.id + ' hit 25% from top of window') 
+// }, {
+//   offset: '25%'
+// })
 
 });
