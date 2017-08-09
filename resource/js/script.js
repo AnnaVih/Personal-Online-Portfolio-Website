e1db1c20bf7e3f8f5d9b1439ee694b92964b4fc3
Anna Vihrogonova <annavihrogonova@yahoo.com>
2017-08-09 14:31:37 +0100

    Add animations on scroll

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	/* For the sticky navigation */
 	$('.js-section-about-me').waypoint(function(direction) {
 		if(direction == 'down') {
 			$('nav').addClass('sticky-nav');
@@ -6,15 +7,58 @@ $(document).ready(function() {
 			$('nav').removeClass('sticky-nav');
 		}
 	},  {
- 	 offset: '60px'
+ 		offset: '60px;'
 	});
 
+	/* Scroll on buttons */
+	$('.js-scroll-to-about-me').click(function() {
+		$('html, body').animate({scrollTop: $('.js-section-about-me').offset().top}, 1000)
+	});
+	$('.js-scroll-to-hire-me').click(function() {
+		$('html, body').animate({scrollTop: $('.js-section-hire-me').offset().top}, 1000)
+	});
 
+	/* Navigation scroll */
+	$(" a[href^='#']").on('click', function(e) {
+   		e.preventDefault();
+	  	var hash = this.hash;
+	    $('html, body').animate({
+	       scrollTop: $(hash).offset().top
+	    }, 1000, function(){
+	       window.location.hash = hash;
+	    });
+	});
 
-// 	var waypoints = $('#handler-first').waypoint(function(direction) {
-//   notify(this.element.id + ' hit 25% from top of window') 
-// }, {
-//   offset: '25%'
-// })
+	/* Animation */
+		//For HEADER
+		$('.js-heading-fadeIn').addClass('animated jackInTheBox');
 
+		$('.js-header-animated').delay(9000).queue(function() {
+			$(this).addClass('animated bounceInDown');
+		});
+
+		// For ABOUT ME section
+		$('.js-about-me-animated').waypoint(function(direction) {
+			$('.js-about-me-animated').addClass('animated bounceInRight');
+		}, {
+			offset: '50%'
+		});
+
+		$('.js-projects-animated').waypoint(function(direction) {
+			$('.js-projects-animated').addClass('animated bounceInLeft');
+		}, {
+			offset: '50%'
+		});
+
+		$('.projects-animated-work').waypoint(function(direction) {
+			$('.projects-animated-work').addClass('animated zoomInDown');
+		}, {
+			offset: '50%'
+		});
+
+		$('.js-hire-me-animated').waypoint(function(direction) {
+			$('.js-hire-me-animated').addClass('animated shake');
+		}, {
+			offset: '80%'
+		});
 });
