9fc8273002ce849a5ded826bf9edcb898cdfeed2
Anna Vihrogonova <annavihrogonova@yahoo.com>
2017-08-28 15:00:58 +0100

    install and add sourcemaps plugin for project

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function () {
 	gulp.src('./resource/css/scss/main.scss')
+		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(autoprefixer())
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./resource/css/css'))
 		.pipe(browserSync.reload({stream: true}));
 });
